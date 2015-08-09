633e879fe94db416e861f6c1445f6b8c66b3fd85
apppur <applepurplediandian@gmail.com>
2015-08-09 11:37:34 +0800

    del comment

--- src/main.cpp
+++ src/main.cpp
@@ -35,17 +35,9 @@ int main(void)
 	glfwSetErrorCallback(error_callback);
 	if (!glfwInit())
 		return (EXIT_FAILURE);
-	/*
-	GLFWmonitor* monitor = glfwGetPrimaryMonitor();
-	const GLFWvidmode* mode = glfwGetVideoMode(monitor);
-	glfwWindowHint(GLFW_RED_BITS, mode->redBits);
-	glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
-	glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
-	glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
-	*/
+
 	window = glfwCreateWindow(width, height, "CPU Ray Tracing", NULL, NULL);
-	// full screen
-	//window = glfwCreateWindow(1048, 640, "Simple example", glfwGetPrimaryMonitor(), NULL);
+
 	if (!window)
 	{
 		glfwTerminate();
@@ -63,33 +55,9 @@ int main(void)
 		//glViewport(0, 0, width, height);
 		glClearColor(0.0, 0.0, 0.0, 1.0);
 		glClear(GL_COLOR_BUFFER_BIT);
-		//glDrawPixels(width, height, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
 		glDrawPixels(width, height, GL_RGBA, GL_UNSIGNED_BYTE, pixels.GetPixels());
 		//glFlush();
 		//Display();
-		/*
-		float ratio;
-		int width, height;
-		glfwGetFramebufferSize(window, &width, &height);
-		ratio = width / (float)height;
-		glViewport(0, 0, width, height);
-		//glClearColor(1.0, 0.0, 1.0, 0.0);
-		glClear(GL_COLOR_BUFFER_BIT);
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();
-		glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-		glMatrixMode(GL_MODELVIEW);
-		glLoadIdentity();
-		glRotatef((float)glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
-		glBegin(GL_TRIANGLES);
-		glColor3f(1.f, 0.f, 0.f);
-		glVertex3f(-0.6f, -0.4f, 0.f);
-		glColor3f(0.f, 1.f, 0.f);
-		glVertex3f(0.6f, -0.4f, 0.f);
-		glColor3f(0.f, 0.f, 1.f);
-		glVertex3f(0.f, 0.6f, 0.f);
-		glEnd();
-		*/
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
